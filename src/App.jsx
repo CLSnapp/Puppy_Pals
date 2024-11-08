bcd50fcd26573668a8f48a4f6d7ad86541573956
CLSnapp <repbandito@hotmail.com>
2024-11-07 21:29:42 -0600

    onClick being added

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,41 @@ import "./App.css";
 import { puppyList } from "./data.js";
 import { useState } from "react";
 
+// function App() {
+//   const [puppies, setPuppies] = useState(puppyList);
+//   const [featPupId, setFeatPupId] = useState(null);
+//   console.log("puppyList: ", puppyList);
+
+//   return (
+//     <>
+//       <div className="App">
+//         {puppies.map((puppy) => {
+//           return <p key={puppy.id}>{puppy.name}</p>;
+          
+//         })}
+//       </div>
+//     </>
+//   );
+// }
+
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log("puppyList: ", puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick() {
+    // click,click,boom
+  }
 
   return (
-    <>
-      <div className="App">
-        {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
-        })}
-      </div>
-    </>
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p 
+          key={puppy.id}
+          onClick={() => { console.log("puppy id: ", puppy.id);}}>{puppy.name}</p>
+        );
+      })}
+    </div>
   );
 }
 
